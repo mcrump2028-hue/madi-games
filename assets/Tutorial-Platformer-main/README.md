4cbeb235cd530b0c07d242448ccb3d4064761026
mcrump2028-hue <mcrump2028@crossroadscollegeprep.org>
2026-02-05 15:03:08 -0600

    adding the game

---
+++ assets/Tutorial-Platformer-main/README.md
@@ -0,0 +1,3 @@
+# Tutorial-Platformer
+ 
+Testing
---
+++ playerjump.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+#Figure out how body is connected to the gravity with a node
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta): 
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
+		
+	
+	
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
