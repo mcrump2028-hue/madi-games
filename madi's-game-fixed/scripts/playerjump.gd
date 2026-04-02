7a555eba7cbaf0b327491a53f10fdf932e666a1b
mcrump2028-hue <mcrump2028@crossroadscollegeprep.org>
2026-04-02 13:48:14 -0500

    Updated player.gd
    changed jumping veloicty form -300 to -400 so player would jumper higher

--- madi's-game-fixed/scripts/playerjump.gd
+++ madi's-game-fixed/scripts/playerjump.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -400.0
 
 #Figure out how body is connected to the gravity with a node
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
