849f8e86d9b2d2e7e340c76d6f5a0eda16024750
mcrump2028-hue <mcrump2028@crossroadscollegeprep.org>
2026-04-09 13:46:20 -0500

    Background to Game Tscn
    Trying to add a background to game tscn

--- madi's-game-fixed/mainmenuguide.gd
+++ madi's-game-fixed/mainmenuguide.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var levelscence : PackedScene
 
+@export var level2scene: PackedScene
+
 func _on_button_pressed() -> void:
 	get_tree(). get_root(). add_child(levelscence.instantiate())
 	$CanvasLayer/Button.hide()
